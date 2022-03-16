3377ec51aa3ffa0e2e307bf5305feab7851d12fb
Dodsie <baileydods@gmail.com>
2022-03-16 15:16:03 -0600

    Pair programming WordSearch

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,29 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+  it("should return false if letter is an empty array", function() {
+    const result = wordSearch([], 'SEINFELD');
+    assert.isFalse(result);
+  });
+  it("should return true the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'I', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'E', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'E', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'R', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'D', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'O', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'G', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WIENERDOG');
+    assert.isFalse(result);
+  });
+  
+  
 });
+
